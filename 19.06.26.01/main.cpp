910e160fa1823d8479661569aed1e8844a1e6bcb
20181105879 <2605753145@qq.com>
2019-06-26 19:09:35 +0800

    Update main.cpp
    1

--- 19.06.26.01/main.cpp
+++ 19.06.26.01/main.cpp
@@ -1,15 +1,93 @@
-//
-//  main.cpp
-//  19.06.26.01
-//
-//  Created by s20181105879 on 2019/6/26.
-//  Copyright © 2019 s20181105879. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <algorithm>
+using namespace std;
+long long s[100000000];
+int main()
+{
+    long long i,t,j;
+    long long count=0,g=0;
+    for(i=0;;i++)
+    {
+        cin>>t;
+        if(t==-1)
+        {
+            if(i==0)
+            {
+                cout<<"NULL";
+                return 0;
+            }
+            break;
+        }
+        else
+        {
+            //           if(s[t]==0)
+            //          {
+            s[t]++;
+            count++;
+            //           }
+        }
+        
+    }
+    for(;;i++)
+    {
+        cin>>t;
+        if(t==-1)
+        {
+            if(i==0)
+            {
+                cout<<"NULL";
+                return 0;
+            }
+            break;
+        }
+        else
+        {
+            //           if(s[t]==0)
+            
+            s[t]++;
+            count++;
+            
+        }
+        
+    }
+    int k=0;
+    i=0;
+    if(count==0)
+    {
+        cout<<"NULL";
+    }
+    for(j=0;;j++)
+    {
+        if(s[j]!=0)
+        {
+            
+            for(g=0;g<s[j];g++)
+            {
+                if(k==0)
+                {
+                    cout<<j;
+                }
+                else
+                    cout<<" "<<j;
+                k=1;
+            }
+            
+            
+            i++;
+        }
+        if(i==count)
+        {
+            return 0;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
